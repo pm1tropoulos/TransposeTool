dd4c058af51ab4baaf35f1765bb4bc74815fea55
pm1tropoulo <pmitropoulos@outlook.com.gr>
2021-07-11 13:47:50 +0300

    Content merged

--- Program.cs
+++ Program.cs
@@ -12,14 +12,12 @@ namespace TransposeTool {
 
         static void Main(string[] args) {
             //another comment
-
             Console.WriteLine("Please insert the csv File path...");
             string path = Console.ReadLine().ToString();
             string ext = Path.GetExtension(path).Remove(0,1);
             Enums.fileType fileType = (Enums.fileType)Enum.Parse(typeof(Enums.fileType),ext,true);
             Console.WriteLine($"I will read the .csv from the path: {path}\n");
             MyFile myFile = new MyFile(fileType, path, ";");
-
             ProcessFactory factory = new ProcessFactory(myFile);
             IFileProcess csvProcess = factory.GetProcess();
             csvProcess.processTranspose(myFile);
