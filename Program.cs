44a12cdd7889d87ad61dbe0c65fc36a716fc30ee
pm1tropoulo <pmitropoulos@outlook.com.gr>
2021-07-11 13:44:11 +0300

    Empty spaces removed

--- Program.cs
+++ Program.cs
@@ -9,22 +9,16 @@ using TransposeTool.ToolsClasses;
 
 namespace TransposeTool {
     class Program {
-
         static void Main(string[] args) {
-
-
             Console.WriteLine("Please insert the csv File path...");
             string path = Console.ReadLine().ToString();
-            string ext = Path.GetExtension(path).Remove(0,1);
-            Enums.fileType fileType = (Enums.fileType)Enum.Parse(typeof(Enums.fileType),ext,true);
+            string ext = Path.GetExtension(path).Remove(0, 1);
+            Enums.fileType fileType = (Enums.fileType)Enum.Parse(typeof(Enums.fileType), ext, true);
             Console.WriteLine($"I will read the .csv from the path: {path}\n");
             MyFile myFile = new MyFile(fileType, path, ";");
-
             ProcessFactory factory = new ProcessFactory(myFile);
             IFileProcess csvProcess = factory.GetProcess();
             csvProcess.processTranspose(myFile);
             }
-
-
         }
     }
